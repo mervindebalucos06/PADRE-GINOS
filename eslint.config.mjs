294ea449b3ea2ece9ee74d4e2c858adeb0fda311
Mervin <mervidebalucos@gmail.com>
2024-11-05 08:16:38 +0800

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,21 @@
 import jss from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  js.configs.recommended,
+  js.configs.recommended, {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  react.reactPlugin.configs.flat["jsx-runtime"],
   {
-    files: ["**/*.js"],
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -15,7 +24,10 @@ export default [
         },
       },
     },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+    },
   },
   prettier,
 ];
- 
