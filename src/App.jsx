6104638083a0cf90bf6b90eb213a01e52d29678f
Mervin <mervidebalucos@gmail.com>
2024-11-07 15:42:06 +0800

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import { Route } from "./routes/__root";
 
 const router = createRouter({ routeTree });
 
-
 const App = () => {
-  const cartHook = useState([]);
   return (
     <StrictMode>
       <RouterProvider router={router} />
     </StrictMode>
-  )
+  );
 };
 
 const container = document.getElementById("root");
