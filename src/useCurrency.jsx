87a36c2c0d0971ef963e26479ceb736c95ff8a84
Mervin <mervidebalucos@gmail.com>
2024-11-08 16:09:32 +0800

    update

---
+++ src/useCurrency.jsx
@@ -0,0 +1,10 @@
+const intl = new Intl.DateTimeFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
+
+export const priceConverter = (price) => intl.format(price);
+export default function useCurrency(price) {
+  return priceConverter(price);
+}
+
